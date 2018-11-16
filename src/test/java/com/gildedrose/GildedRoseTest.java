7a09a2013112319fad9462386edffd2c5a9e5a9f
Administrator <git commit -m "first commit">
2018-11-16 15:13:06 +0900

    [완료] 커버리지 100%

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -110,44 +110,75 @@ public class GildedRoseTest {
 
 		// 같이 해보기 1
 		// arrange (given)
-
+		Item[] items= new Item[] {new Item(BACKSTAGE_PASS, 0, 51) };
+		GildedRose gildedRose= new GildedRose(items);
+		
+		
 		// act (when)
+		gildedRose.updateQuality();
+		
 
 		// assert (then)
-
+		assertEquals(BACKSTAGE_PASS, items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(0, items[0].quality);
 	}
 
 	@Test
 	public void BackstagePass아이템_판매기간15_품질0_테스트() {
 
-		// 같이 해보기 2
+		// 같이 해보기 1
 		// arrange (given)
-
+		Item[] items= new Item[] {new Item(BACKSTAGE_PASS, 15, 0) };
+		GildedRose gildedRose= new GildedRose(items);
+		
+		
 		// act (when)
+		gildedRose.updateQuality();
+		
 
 		// assert (then)
+		assertEquals(BACKSTAGE_PASS, items[0].name);
+		assertEquals(14, items[0].sellIn);
+		assertEquals(1, items[0].quality);
 	}
 
 	@Test
 	public void Sulfuras아이템_판매기간_마이너스1_품질5_테스트() {
 
-		// 실습 1
+		// 같이 해보기 1
 		// arrange (given)
-
+		Item[] items= new Item[] {new Item(SULFURAS, -1, 5) };
+		GildedRose gildedRose= new GildedRose(items);
+		
+		
 		// act (when)
+		gildedRose.updateQuality();
+		
 
 		// assert (then)
+		assertEquals(SULFURAS, items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(5, items[0].quality);
 	} 
 
 	@Test
 	public void AgedBrie아이템_판매기간0_품질55_테스트() {
 
-		// 실습 2
+		// 같이 해보기 1
 		// arrange (given)
-
+		Item[] items= new Item[] {new Item(AGED_BRIE, 0, 55) };
+		GildedRose gildedRose= new GildedRose(items);
+		
+		
 		// act (when)
+		gildedRose.updateQuality();
+		
 
 		// assert (then)
+		assertEquals(AGED_BRIE, items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(55, items[0].quality);
 
 	}
 
