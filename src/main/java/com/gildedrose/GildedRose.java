c96bcf8d62bcbebb88ba2163dd5f862ef5808b6f
Administrator <git commit -m "first commit">
2018-11-16 15:59:09 +0900

    [완료] 메서드 추출

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,60 +15,16 @@ class GildedRose {
 		for (int i = 0; i < items.length; i++) {
 			Item item = items[i];
 			if (item.name.equals(AGED_BRIE)) {
-				if (item.quality < MAX_QUALITY) {
-					item.quality = item.quality + 1;
-				}
-
-				if (item.sellIn < 1) {
-					if (item.quality < MAX_QUALITY) {
-						item.quality = item.quality + 1;
-					}
-				}
+				updateQuailityForAgedBrie(item);
 
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
-				if (item.quality < MAX_QUALITY) {
-					item.quality = item.quality + 1;
-
-					if (item.sellIn < 11) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-
-					if (item.sellIn < 6) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-
-				}
-
-				if (item.sellIn < 1) {
-					item.quality = item.quality - item.quality;
-				}
+				updateQualityForBackstagePass(item);
 
 			} else if (item.name.equals(SULFURAS)) {
-				if (item.quality > 0) {
-
-				}
-				//
-
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-
-					}
-				}
-
+				updateQualityForSulfuras();
+				
 			} else {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
-
-				if (item.sellIn < 1) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				}
+				updateQualityForNormal(item);
 			}
 
 			if (item.name.equals(SULFURAS)) {
@@ -76,15 +32,56 @@ class GildedRose {
 			} else {
 				item.sellIn = item.sellIn - 1;
 			}
+		}
+	}
 
-			if (item.name.equals(AGED_BRIE)) {
+	private void updateQualityForSulfuras() {
+		System.out.println("");
+	}
 
-			} else if (item.name.equals(BACKSTAGE_PASS)) {
+	private void updateQualityForNormal(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
 
-			} else if (item.name.equals(SULFURAS)) {
+		if (item.sellIn < 1) {
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
+			}
+		}
+	}
 
-			} else {
+	private void updateQualityForBackstagePass(Item item) {
+		if (item.quality < MAX_QUALITY) {
+			item.quality = item.quality + 1;
 
+			if (item.sellIn < 11) {
+				if (item.quality < MAX_QUALITY) {
+					item.quality = item.quality + 1;
+				}
+			}
+
+			if (item.sellIn < 6) {
+				if (item.quality < MAX_QUALITY) {
+					item.quality = item.quality + 1;
+				}
+			}
+
+		}
+
+		if (item.sellIn < 1) {
+			item.quality = item.quality - item.quality;
+		}
+	}
+
+	private void updateQuailityForAgedBrie(Item item) {
+		if (item.quality < MAX_QUALITY) {
+			item.quality = item.quality + 1;
+		}
+
+		if (item.sellIn < 1) {
+			if (item.quality < MAX_QUALITY) {
+				item.quality = item.quality + 1;
 			}
 		}
 	}
