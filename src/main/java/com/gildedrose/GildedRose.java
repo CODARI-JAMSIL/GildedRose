df077d238f878b4b765e5fc841c9b520be6837cd
Administrator <git commit -m "first commit">
2018-11-16 15:50:48 +0900

    [완료] 분기문 합치기

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,6 +18,13 @@ class GildedRose {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
 				}
+
+				if (item.sellIn < 1) {
+					if (item.quality < MAX_QUALITY) {
+						item.quality = item.quality + 1;
+					}
+				}
+
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
@@ -35,14 +42,32 @@ class GildedRose {
 					}
 
 				}
+
+				if (item.sellIn < 1) {
+					item.quality = item.quality - item.quality;
+				}
+
 			} else if (item.name.equals(SULFURAS)) {
 				if (item.quality > 0) {
 
 				}
 				//
+
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+
+					}
+				}
+
 			} else {
 				if (item.quality > 0) {
 					item.quality = item.quality - 1;
+				}
+
+				if (item.sellIn < 1) {
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
+					}
 				}
 			}
 
@@ -53,30 +78,12 @@ class GildedRose {
 			}
 
 			if (item.name.equals(AGED_BRIE)) {
-				if (item.sellIn < 0) {
-					if (item.quality < MAX_QUALITY) {
-						item.quality = item.quality + 1;
-					}
-				}
 
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
-				if (item.sellIn < 0) {
-					item.quality = item.quality - item.quality;
-				}
 
 			} else if (item.name.equals(SULFURAS)) {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-
-					}
-				}
 
 			} else {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				}
 
 			}
 		}
